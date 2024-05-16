3ffa336d6739a3f0a9f65723ec251c27924ded1d
mutumagitonga <mutumamwiti11@gmail.com>
2024-05-16 03:40:17 +0300

    feat: Add function to split the extracted PDF text string at every paragraph & return the paragraph strings in a list

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+import re
+
 from dotenv import load_dotenv
 from pypdf import PdfReader
 
@@ -9,7 +11,7 @@ gemini_api_key = os.environ.get("GEMINI_API_KEY")
 #########################
 # STAGE 1: INDEXING DATA #
 ##########################
-# Part 1.1: Loading data
+# => Part 1.1: Loading data
 
 
 def read_and_extract_text_from_pdf(file_path):
@@ -30,5 +32,25 @@ def read_and_extract_text_from_pdf(file_path):
 
 
 # Call the text scrape & read function
-all_pdf_text = read_and_extract_text_from_pdf(file_path="./docs/state_of_the_union.pdf")
+extracted_pdf_text = read_and_extract_text_from_pdf(file_path="./docs/state_of_the_union.pdf")
 
+
+# => Part 1.2: Splitting data
+# The data is split into many chunks to ensure shorter context windows (recipe for better accuracy)
+# However, the reason above doesn't help much for Gemini Pro since it currently has a context window of 1M tokens
+# Thus, the splitting speeds up the 1st stage of the RAG process when a user prompt is used to search for ...
+# ... semantically (relevant) content from the user text corpus before creating an augmented prompt for the LLM API
+
+
+def split_text_corpus_every_paragraph(text_corpus):
+    """
+    This function splits the previously extracted text string at every paragraph using regex marker "\n \n"
+    :param text_corpus: (type: str) The text string to split into individual paragraphs
+    :return: (List[str]) Returns a list of strings representing split paragraphs
+    """
+    split_paragraphs = re.split('\n \n', text_corpus)
+    return [paragraph for paragraph in split_paragraphs if paragraph != ""]  # List comprehension to create list
+
+
+# Call the paragraph-splitting function to return a list of paragraph strings
+paragraph_chunks = split_text_corpus_every_paragraph(text_corpus=extracted_pdf_text)
